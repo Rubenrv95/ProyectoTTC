9c6e214ff757b2a2b86c36e511fb5f327e32f2de
Matias Pizarro <pinkystylee98@gmail.com>
2021-05-13 19:55:05 -0400

    xd

--- Arduino_Proyecto_TTC/Arduino_Proyecto_TTC.ino
+++ Arduino_Proyecto_TTC/Arduino_Proyecto_TTC.ino
@@ -15,18 +15,17 @@ DHT dht(DHTPIN,DHTTYPE);
 #define luz8 30
 
 //Variable LDR
-const int LDR = A0;
+#define LDR  A0
+int valorAnterior = 0;
 
 //
-#define led 31
+#define luzP 31
 
 void setup() {
   Serial.begin(9600);
 
   //Inicializa DHT22
   dht.begin();
-  //Inicializa LDR
-  pinMode(LDR, INPUT);
   
   //Inicializa Pines LED
   pinMode(luz1, OUTPUT);
@@ -38,18 +37,90 @@ void setup() {
   pinMode(luz7, OUTPUT);
   pinMode(luz8, OUTPUT);
   
-  pinMode(led, OUTPUT);   
+  pinMode(luzP, OUTPUT);
+    Serial.println("Ingresa comando:");
 }
 
 void loop() {
-  prenderLuces();
-  obtenerTemperatura();
+  //prenderLuces();
+  //obtenerTemperatura();
+  //obtenerLDR();
+  //obtenerHumedad();
+  //delay(1000);
+  //apagarLuces();
+  //delay(1000);
+  menu();
   obtenerLDR();
-  obtenerHumedad();
-  delay(1000);
-  apagarLuces();
-  delay(1000);
-  
+}
+
+void menu(){
+  if (Serial.available()  ){
+    String opcion = Serial.readString();
+    opcion.trim();
+    if(opcion == "luz 1 on"){
+      digitalWrite(luz1,HIGH);
+    }
+    else if(opcion == "luz 1 off"){
+      digitalWrite(luz1,LOW);
+    }
+    else if(opcion == "luz 2 on"){
+      digitalWrite(luz2,HIGH);
+    }
+    else if(opcion == "luz 2 off"){
+      digitalWrite(luz2,LOW);
+    }
+    else if(opcion == "luz 3 on"){
+      digitalWrite(luz3,HIGH);
+    }
+    else if(opcion == "luz 3 off"){
+      digitalWrite(luz3,LOW);
+    }
+    else if(opcion == "luz 4 on"){
+      digitalWrite(luz4,HIGH);
+    }
+    else if(opcion == "luz 4 off"){
+      digitalWrite(luz4,LOW);
+    }
+    else if(opcion == "luz 5 on"){
+      digitalWrite(luz5,HIGH);
+    }
+    else if(opcion == "luz 5 off"){
+      digitalWrite(luz5,LOW);
+    }
+    else if(opcion == "luz 6 on"){
+      digitalWrite(luz6,HIGH);
+    }
+    else if(opcion == "luz 6 off"){
+      digitalWrite(luz6,LOW);
+    }
+    else if(opcion == "luz 7 on"){
+      digitalWrite(luz7,HIGH);
+    }
+    else if(opcion == "luz 7 off"){
+      digitalWrite(luz7,LOW);
+    }
+    else if(opcion == "luz 8 on"){
+      digitalWrite(luz8,HIGH);
+    }
+    else if(opcion == "luz 8 off"){
+      digitalWrite(luz8,LOW);
+    }
+    else if (opcion == "temperatura"){
+      obtenerTemperatura();
+    }
+    else if (opcion == "humedad"){
+      obtenerHumedad();
+    }
+    else if(opcion == "luces on"){
+      prenderLuces();
+    }
+    else if(opcion == "luces off"){
+      apagarLuces();
+    }
+    else{
+      Serial.println("Comando no reconocido");
+    }
+  }
 }
 
 void prenderLuces(){
@@ -94,5 +165,14 @@ void obtenerHumedad(){
 
 void obtenerLDR(){
   int valor = analogRead(LDR);
-  Serial.println(valor);
+  delay(100);
+  if( valorAnterior != valor){
+    valorAnterior = valor;
+    if(valorAnterior <= 100){
+      digitalWrite(luzP,HIGH);
+    }
+    else{
+      digitalWrite(luzP,LOW);
+    }
+  }
 }
